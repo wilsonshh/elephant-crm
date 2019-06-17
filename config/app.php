e907994ff88f0db55489e42cbd28fca1ea212204
Wilson <wilson.shrestha@gmail.com>
2019-06-17 10:58:06 +0100

    removed class

--- config/app.php
+++ config/app.php
@@ -162,12 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        /*
-         * Package Service Providers...
-         */
-        TomLingham\Searchy\SearchyServiceProvider::class,
-
-
+    
         /*
          * Application Service Providers...
          */
